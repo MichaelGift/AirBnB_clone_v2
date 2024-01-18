234c7eced8073d623b43f07c6cd4e0a093ed96dc
MichaelGift <simiyugift@outlook.com>
2024-01-18 04:26:53 +0300

    refactor: Change attrs based on storage
    Add getter for all cities if storage is not db

--- models/state.py
+++ models/state.py
@@ -3,16 +3,31 @@
 from sqlalchemy import Column, String
 from sqlalchemy.orm import relationship
 
+import models
 from models.base_model import BaseModel, Base
+from models.city import City
 
 
 class State(BaseModel, Base):
     """ State class """
-
-    __tablename__ = 'states'
-    name = Column(String(128), nullable=False)
-    cities = relationship("City", backref="state")
+    if models.storage_type == "db":
+        __tablename__ = 'states'
+        name = Column(String(128), nullable=False)
+        cities = relationship("City", backref="state")
+    else:
+        name = ""
 
     def __init__(self, *args, **kwargs):
         """`Initializes a state object"""
         super.__init__(*args, **kwargs)
+
+    if models.storage_type != "db":
+        @property
+        def cities(self):
+            """Gets all cities related to this state"""
+            result = []
+            all_cities = models.storage.all(City)
+            for city in all_cities.values():
+                if city.state_id == self.id:
+                    result.append(city)
+            return result
