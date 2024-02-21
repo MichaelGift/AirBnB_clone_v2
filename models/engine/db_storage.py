122178660f6db27aa4485fd0e109b253aafd9ed7
MichaelGift <simiyugift@outlook.com>
2024-02-22 01:34:00 +0300

    Feat: Update to remove and reload on clode

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -1,7 +1,6 @@
 """
 Houses the DBStorage class
 """
-import models
 from os import getenv
 
 from sqlalchemy import create_engine
@@ -72,3 +71,7 @@ class DBStorage:
                            (bind=self.__engine, expire_on_commit=False))
         Session = scoped_session(session_factory)
         self.__session = Session
+
+    def close(self):
+        """call remove() method on the private session attribute"""
+        self.__session.remove()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -61,3 +61,7 @@ class FileStorage:
             unique_id = obj.__class__.__name__ + '.' + obj.id
             if unique_id in FileStorage.__objects:
                 del FileStorage.__objects[unique_id]
+
+    def close(self):
+        """call reload() method for deserializing the JSON file to objects"""
+        self.reload()
